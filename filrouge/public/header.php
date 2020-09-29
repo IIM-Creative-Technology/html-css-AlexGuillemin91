cf1a40336d0d24b66f727f53cbb7735d390a6f25
AlexGuillemin91 <70761911+AlexGuillemin91@users.noreply.github.com>
2020-09-29 15:10:43 +0200

    Projet fil rouge
    les 3 pages principales sont:
    -index.php
    -sondage.php
    -classement.php

---
+++ filrouge/public/header.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="menu">
+        <div class="home">
+            <a href="#" class="logo"><img src="img/logo%20sondage.png" alt="logo"></a>
+            <a href="#"><img src="img/home-solid.svg">
+                <h2>Accueil</h2>
+            </a>
+        </div>
+        <div class="classement">
+            <a href="#"><img src="img/trophy-solid.svg">
+                <h2>Classement</h2>
+            </a>
+        </div>
+        <div class="connexion">
+            <a href="#"><img src="img/user-solid.svg">
+                <h2>Connexion</h2>
+            </a>
+        </div>
+    </div>
+</header>
